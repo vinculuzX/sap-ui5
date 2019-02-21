35044bca691a8af68a438d2d7e496b5bdaff512d
vinculuzx <vinculuzx.app@gmail.com>
2019-02-21 11:01:08 +0000

    bootstrap sap ui5

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+ ], function (UIComponent) {
+    "use strict";
+    return UIComponent.extend("", {
+ 
+       init : function () {
+          // call the init function of the parent
+          UIComponent.prototype.init.apply(this, arguments);
+       }
+    });
+ });
